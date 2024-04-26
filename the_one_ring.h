555f664b478fa55a51c19699ff81bcf255a022e0
rondubi <rdubinsky@live.com>
2024-04-26 11:00:17 -0700

    Added clang format

--- the_one_ring.h
+++ the_one_ring.h
@@ -1,6 +1,6 @@
 /* Hacky attempt at a ring buffer implementation in C */
 
-struct RingBuffer
+struct ringbuffer // MY PRECIOUSSSS
 {
         void * storage = nullptr;
         int32_t capacity = 0;
@@ -8,3 +8,15 @@ struct RingBuffer
         int32_t end = 0;
 };
 
+// NOTE: these two methods return the number of elements inserted
+// TODO: error handling
+int32_t insert_bulk(ringbuffer * r, void * new_elems, int32_t count)
+{
+        if (r.end == r.start)
+                return 0;
+}
+
+int32_t insert_elem(ringbuffer * r, void * new_elem)
+{
+        return insert_bulk(r, new_elem, 1);
+}
