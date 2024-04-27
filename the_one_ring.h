64d89cd573432a23e5b5e8c74ccb91fc1d1fcb04
rondubi <rdubinsky@live.com>
2024-04-26 20:39:48 -0700

    First draft of bulk insert

--- the_one_ring.h
+++ the_one_ring.h
@@ -18,6 +18,34 @@ void rb_push_elem(ringbuffer * r, void * new_elem)
         r->end %= r->capacity;
 }
 
+void rb_push_bulk(ringbuffer * r, void ** new_elems, int32_t count)
+{
+        if (r->start == r->end)
+                return;
+
+        // One-part copy
+        if (r->start > r->end)
+        {
+                // No max function in C lmao
+                const int32_t effective_count = count > r->start - r->end ? r->start - r->end : count;
+                memcpy(r->storage + r->end, new_elems, effective_count * sizeof(void *));
+                // TODO: break into a function
+                r->end = (r->end + effective_count) % r->capacity;
+                return;
+        }
+
+        // Two-part copy
+        const int32_t first_copy_count = r->capacity - r->end > count ? count : r->capacity - r->end;
+        memcpy(r->storage + r->end, new_elems, first_copy_count * sizeof(void *));
+        r->end = (r->end + effective_count) % r->capacity;
+        if (first_copy_count >= count)
+                return;
+
+        const int32_t second_copy_count = r->start < count - first_copy_count ? r->start : count - first_copy_count;
+        memcpy(r->storage, new_elems + first_copy_count, second_copy_count * sizeof(void *));
+        r->end = (r->end + effective_count) % r->capacity;
+}
+
 // Returns popped element
 // TODO: error handling
 void * rb_pop_elem(ringbuffer * r)
