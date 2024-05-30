2302259eb3e4225f7b08cad74aefd6aa425fc79d
rondubi <rdubinsky@live.com>
2024-05-29 20:13:19 -0700

    Ported ipfw

--- ipfw.hh
+++ ipfw.hh
@@ -1,13 +1,13 @@
-#include <stdlib.h>
+#include <cstdlib>
 
-enum action_t
+enum action
 {
         ALLOW,
         DENY
         // TODO: more actions
 };
 
-struct packet_t
+struct packet
 {
         int32_t ipv4_from;
         int32_t ipv4_to;
@@ -17,11 +17,11 @@ void rules_init();
 
 // NOTE: should return status
 // NOTE: does not prevent overriding of existing rules
-int rule_add(int rule_number, enum action_t action, int32_t ipv4_from, int32_t ipv4_to);
+int rule_add(int rule_number, action_t action, int32_t ipv4_from, int32_t ipv4_to);
 
 // NOTE: should return status
 // TODO: wtf is a packet_t
-int rule_apply(struct packet_t packet);
+int rule_apply(packet_t packet);
 
 // TODO: pipe and queue configuration
 
