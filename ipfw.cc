00c8bb1bc573f5407b2e209c34b0f6d73ce533f5
rondubi <rdubinsky@live.com>
2024-05-29 20:23:08 -0700

    Updating code

--- ipfw.cc
+++ ipfw.cc
@@ -1,45 +1,25 @@
 #include "ipfw.h"
 
-#include <assert.h>
-#include <stdlib.h>
+#include <cassert>
+#include <cstdlib>
 
-int pipe_send_packet(struct packet_t packet);
+int pipe_send_packet(packet packet);
 
 // Some site with the IPFW BNF: http://info.iet.unipi.it/~luigi/ip_dummynet/original.html
 
-// NOTE: assume for now that all protocols are IPv4
-// NOTE: assume for now that rules are not probabilistic
-// NOTE: assume rules are only on dummynet
-
-// TODO: do we need to know what pipes are valid?
-struct rule
-{
-        // NOTE: no pattern matching at the moment
-        int32_t ipv4_from;
-        int32_t ipv4_to;
-        enum action_t action;
-};
-
 #define RULE_TABLE_CAPACITY (1 << 16  - 1)
-static struct rule * rule_table;
 
-void rules_init()
-{
-        rule_table = malloc(sizeof(struct rule) * RULE_TABLE_CAPACITY);
-        memset(rule_table, -1, RULE_TABLE_CAPACITY);
-}
-
-int rule_add(int32_t rule_number, enum action_t action, int32_t ipv4_from, int32_t ipv4_to)
+int Rules::rule_add(int32_t rule_number, action action, int32_t ipv4_from, int32_t ipv4_to)
 {
         assert(RULE_TABLE_CAPACITY > rule_number);;
 
-        rule_table[rule_number] = (struct rule) {.action = action, .ipv4_from = ipv4_from, .ipv4_to = ipv4_to, };
+        rule_table[rule_number] = Rules::RUle {.action = action, .ipv4_from = ipv4_from, .ipv4_to = ipv4_to, };
 
         return 0;
 }
 
 // NOTE: return 0 if no match, return status otherwise
-int try_match(struct packet_t packet, int i)
+int try_match(packet_t packet, int i)
 {
         if (!(packet.ipv4_from == rule_table[i]->ipv4_from && packet.ipv4_to == rule_table[i]->ipv4_to))
                 return 0;
@@ -47,25 +27,28 @@ int try_match(struct packet_t packet, int i)
         return perform_action(packet, i);
 }
 
-int is_uninitialized_rule(int i)
+int Rules::rule_apply(packet packet) const
 {
-        return rule_table[i]->ipv4_from == -1 && rule_table[i]->ipv4_to == -1;
-}
-
-int rule_apply(packet_t packet)
-{
-        for (int i = 0; i < RULE_TABLE_CAPACITY; ++i)
+//        for (int i = 0; i < RULE_TABLE_CAPACITY; ++i)
+//        {
+//                if (is_uninitialized_rule(i))
+//                {
+//                        continue;
+//                }
+//                
+//                const int result = try_match(packet, i);
+//                if (result != 0)
+//                        return result;
+//        }
+//
+//        return default_action(packet);
+        for (const auto & [rnum, rule] : rules_table)
         {
-                if (is_uninitialized_rule(i))
+                if (rule.ipv4_from == packet.ipv4_from && rule.ipv4_to == packet.ipv4_to)
                 {
-                        continue;
+                        // TODO: handle other actions
+                        return pipe_send_packet(packet);
                 }
-                
-                const int result = try_match(packet, i);
-                if (result != 0)
-                        return result;
         }
-
-        return default_action(packet);
 }
 
--- ipfw.hh
+++ ipfw.hh
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <map>
 
 enum action
 {
@@ -13,15 +14,36 @@ struct packet
         int32_t ipv4_to;
 };
 
-void rules_init();
+struct Rules
+{
+        Rules() : rules_table();
 
-// NOTE: should return status
-// NOTE: does not prevent overriding of existing rules
-int rule_add(int rule_number, action_t action, int32_t ipv4_from, int32_t ipv4_to);
+        // NOTE: should return status
+        // NOTE: does not prevent overriding of existing rules
+        int rule_add(int rule_number, action_t action, int32_t ipv4_from, int32_t ipv4_to);
 
-// NOTE: should return status
-// TODO: wtf is a packet_t
-int rule_apply(packet_t packet);
+        // NOTE: should return status
+        // TODO: wtf is a packet_t
+        int rule_apply(packet_t packet);
+        // TODO: pipe and queue configuration
 
-// TODO: pipe and queue configuration
+private:
+        // NOTE: assume for now that all protocols are IPv4
+        // NOTE: assume for now that rules are not probabilistic
+        // NOTE: assume rules are only on dummynet
+
+        // TODO: do we need to know what pipes are valid?
+        struct Rule
+        {
+                // NOTE: no pattern matching at the moment
+                int32_t ipv4_from;
+                int32_t ipv4_to;
+                action action;
+
+                // TODO: matches func and regex support
+                // bool matches(packet p) const;
+        };
+
+        std::map<Rule> rules_table;
+};
 
