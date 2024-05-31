faf2c466e6daba7be45a22477b53d72f24b3689f
rondubi <rdubinsky@live.com>
2024-05-31 15:59:14 -0700

    Tests for pattern match

--- ipfw_test.cc
+++ ipfw_test.cc
@@ -63,8 +63,25 @@ bool test_pattern_match_or()
         const int res1 = r.apply_rules({1, 1});
         const int status1 = status;
         const int res2 = r.apply_rules({2, 1});
+        const int status2 = status;
+        status = 0;
+        const int res3 = r.apply_rules({3, 1});
 
-        return res1 == 0 && status1 == 13 && res2 == 0 && status == 13;
+        return res1 == 0 && status1 == 13 && res2 == 0 && status2 == 13 && res3 == FAIL && status == 0;
+}
+
+bool test_pattern_match_not()
+{
+        int status = 0;
+
+        Rules r([&](packet p){ status = 13; return 0; });
+
+        r.add(1, action::DENY, new Not(new ExactMatch(1)), new ExactMatch(1));
+        const int res1 = r.apply_rules({1, 1});
+        const int status1 = status;
+        const int res2 = r.apply_rules({2, 1});
+
+        return res1 == FAIL && status1 == 0 && res2 == DENIED && status == 0;
 }
 
 int main()
@@ -83,6 +100,9 @@ int main()
 
         assert(test_pattern_match_or());
         printf("Finished pattern match OR test!\n");
+
+        assert(test_pattern_match_not());
+        printf("Finished pattern match NOT test!\n");
 
         return 0;
 }
